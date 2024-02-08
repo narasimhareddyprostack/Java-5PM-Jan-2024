78e24e1b686ab974ab7e6e75866ddbd06b061ab7
narasimhareddyprostack <narasimhareddyk18@gmail.com>
2024-02-08 18:36:39 +0530

    tst

---
+++ Access Modifiers/PackageEx/TestOne.java
@@ -0,0 +1,7 @@
+package pack1;
+
+class TestOne{
+    public static void main(String[] args){
+        System.out.println("TestOne class -main method");
+    }
+}
---
+++ Access Modifiers/packageEx1/A.java
@@ -0,0 +1,9 @@
+package pack1;
+public class A{
+    void m1(){
+        System.out.println("pack1 - public class A - m1 public method");
+    }
+    void m2(){
+         System.out.println("pack1 -public class A - m2 default method");
+    }
+}
